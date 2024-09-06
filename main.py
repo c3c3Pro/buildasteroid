b88615e8e2639c16296f153dc255deb6c6b75251
c3c3Pro <c3lina.bitari@gmail.com>
2024-09-06 22:29:22 +0400

    initialised a pygame screen

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-#import pygame
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+# import everything from a module
+# into the current file
+from constants import *
 
 
-print("Starting asteroids!")
+def main(): 
+
+    #initialise pygame
+    pygame.init()
+    #new GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        #fill the screen with black color
+        pygame.Surface.fill(screen,(0, 0, 0), rect=None)
+        #update the full display surface to the screen 
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")    
+
+if __name__ == "__main__":
+    main()
