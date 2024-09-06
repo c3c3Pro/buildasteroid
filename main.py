54af6e4968f755861997b4b178badb39e3458ead
c3c3Pro <c3lina.bitari@gmail.com>
2024-09-06 22:55:03 +0400

    adjusted the frame rate to 60

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
     #new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         #fill the screen with black color
         pygame.Surface.fill(screen,(0, 0, 0), rect=None)
@@ -22,7 +25,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #fps is 60. the delta time is converted from milliseconds to seconds
+        dt = clock.tick(60) / 1000 
 
+    
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
