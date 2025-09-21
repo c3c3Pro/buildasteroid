55fd14452fdbb7dade620ae33706bd72aa6603e7
c3c3Pro <c3lina.bitari@gmail.com>
2025-09-21 22:08:24 +0400

    fixed the bug with camera in __init__

--- camera.py
+++ camera.py
@@ -2,7 +2,7 @@ import pygame
 class Camera:
     def __init__(self, screen_width, screen_height):
         self.offset = pygame.Vector2(0, 0)
-        self.screen_center = pygame.Vector2(screen_width / 2, screen_height /2)
+        self.screen_center = pygame.Vector2(screen_width, screen_height)
         self.target = None
 
     def follow_camera(self, target):
@@ -12,11 +12,11 @@ class Camera:
 
     def update(self):
         if self.target is not None:
-            self.offset.x = self.target.position.x - self.screen_center.x
-            self.offset.y = self.target.position.y - self.screen_center.y
+            self.offset.x =  self.screen_center.x - self.target.position.x
+            self.offset.y =  self.screen_center.y - self.target.position.y
 
     def apply(self, position):
         # convert world position to the relative position of an object
         # screen position = world position - camera offset
-        return position - self.offset
+        return position + self.offset
     
